860c9ee0cb3effa850d36e804e131691957bc8b9
Sydney <Sydneychristopherjnr@gmail.com>
2025-07-24 04:24:16 +0100

    interactive cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -120,6 +120,14 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       });
     }
 
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    })
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+    console.log(cartQuantity);
     console.log(cart);
   });
 });
